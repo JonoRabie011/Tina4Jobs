27de62bfe7217f64f36ee4ba777b75c62221928b
Jonathan Rabie <jonathan.rabie@toss-group.com>
2025-02-11 08:40:08 +0100

    Added new Kafka Containers

--- Tina4Job/Tina4Queue/Tina4JobFactory.php
+++ Tina4Job/Tina4Queue/Tina4JobFactory.php
@@ -7,11 +7,10 @@ class Tina4JobFactory
     public static function createQueueDriver(): Tina4DatabaseJob|Tina4RedisJob
     {
         $driver = $_ENV['QUEUE_DRIVER'] ?? 'database';
-//        $driver = $_ENV['QUEUE_DRIVER'] ?? 'redis';
 
         return match ($driver) {
             'redis' => new Tina4RedisJob(),
-            'kafka' => new Tina4KafkaJob(),
+//            'kafka' => new Tina4KafkaJob(),
             default => new Tina4DatabaseJob(),
         };
     }
